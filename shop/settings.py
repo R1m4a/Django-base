f71fb782197be78e991e4ea3778f7d33360a36d8
R1m4a <rima.neroo@gmail.com>
2025-07-27 12:44:25 +0200

    ended first model, registered in admin(added full project in commit)

--- shop/settings.py
+++ shop/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    "main",
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Ljubljana'
 
 USE_I18N = True
 
